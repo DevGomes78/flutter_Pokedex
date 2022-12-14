c12181cf32e8cfc60fa3129c12cb161d5cde9a49
usuario <gomes.amilson@gmail.com>
2022-12-14 02:29:10 -0300

    criado arquivo error_constants.dart

--- lib/constants/url_constants.dart
+++ lib/constants/url_constants.dart
@@ -1,4 +1,4 @@
 class ServiceUrl{
   static const BaseUrl =  'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
-  static const ApiError = 'Erro ao acessar os dados';
+
 }
